8d08c4314beeddf33a0197136fd51bf793133f22
alecooper20 <62663425+alecooper20@users.noreply.github.com>
2020-12-03 00:49:45 -0300

    Add files via upload

---
+++ components/FlexBoxItem/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './index.css';
+
+
+function FlexBoxItem({ children }){
+
+
+    return <div className="flex-item">{children}</div>;
+
+
+
+}
+
+
+export default FlexBoxItem;
